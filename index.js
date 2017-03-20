3a16f8ce5f99372f26e9a52c4f9ffaa97f531dd4
Sjur Bakka <barsju@gmail.com>
2017-03-20 11:06:22 +0100

    Implement undo/redo

--- index.js
+++ index.js
@@ -26,17 +26,41 @@ var filename = args._[args._.length - 1];
 
 var file = new FileHandle(filename);
 
-let state;
+const undoStack = [];
+const redoStack = [];
+
+function state() {
+  if (undoStack.length > 0) {
+    return undoStack[undoStack.length - 1];
+  } else {
+    return null;
+  }
+}
 
 function onKey(key, origKey) {
   if (key === 'quit') {
-    writeAndExit(rebaseFile.toFile(state));
+    writeAndExit(rebaseFile.toFile(state()));
   } else if (key === 'abort') {
     writeAndExit('');
+  } else if (key === 'undo') {
+    if (undoStack.length > 1) {
+      const oldState = undoStack.pop();
+      const newState = state();
+      terminal.render(oldState, newState, key, origKey);
+      redoStack.push(oldState);
+    }
+  } else if (key === 'redo') {
+    if (redoStack.length > 0) {
+      const oldState = state();
+      const newState = redoStack.pop();
+      terminal.render(oldState, newState, key, origKey);
+      undoStack.push(newState);
+    }
   } else {
-    const newState = reduce(state, key);
-    terminal.render(state, newState, key, origKey);
-    state = newState;
+    const oldState = state();
+    const newState = reduce(oldState, key);
+    terminal.render(oldState, newState, key, origKey);
+    undoStack.push(newState);
   }
 }
 
@@ -46,8 +70,9 @@ const terminal = new Terminal(term, {
 });
 
 file.read().then((data) => {
-  state = rebaseFile.toState(data);
+  const state = rebaseFile.toState(data);
   terminal.render(null, state, '', '');
+  undoStack.push(state);
 }).catch((err) => {
   exit(err || 'Failed to read file');
 });
