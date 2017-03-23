c8fe47a27e2234deaccdbd4f7a26b07b4ad4494e
Sjur Bakka <barsju@gmail.com>
2017-03-23 08:28:30 +0100

    Test file handler

---
+++ test/file-handle.spec.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const Handler = require('../lib/file-handle');
+const fs = require('fs');
+
+describe('File handler', function () {
+
+  describe('read', function () {
+    it('should read file', function () {
+      const handler = new Handler('test/testfiles/testfile.txt');
+      return expect(handler.read()).to.eventually.equal('Hello\nWorld\n');
+    });
+
+    it('should reject failure', function () {
+      const handler = new Handler('notExistingFile');
+      return expect(handler.read()).to.be.rejected;
+    });
+
+    it('should reject empty file', function () {
+      const handler = new Handler('test/testfiles/emptyfile.txt');
+      return expect(handler.read()).to.be.rejected;
+    });
+  });
+
+  describe('write', function () {
+
+    const file = 'test/testfiles/writtenfile.txt';
+
+    function remove(file, done) {
+      fs.unlink(file, (err) => {
+        //Ignore if file not exists
+        if (err && err.code !== 'ENOENT') {
+          done(err);
+        } else {
+          done();
+        }
+      });
+    }
+
+    afterEach(function (done) {
+      remove(file, done);
+    });
+
+    beforeEach(function (done) {
+      remove(file, done);
+    });
+
+    it('should write to file', function () {
+      const handler = new Handler(file);
+      const str = 'Foo\nBar\n';
+      return handler.write(str).then(() => {
+        return expect(handler.read()).to.be.eventually.equal(str);
+      });
+    });
+
+    it('should reject on faliure', function () {
+      const handler = new Handler('test/testfiles/readonlyfile.txt');
+      return expect(handler.write('foo')).to.be.rejected;
+    });
+  });
+
+
+
+});
--- test/setup.js
+++ test/setup.js
@@ -2,4 +2,5 @@
 var chai = require('chai');
 global.expect = chai.expect;
 chai.use(require('chai-sinon'));
+chai.use(require('chai-as-promised'));
 global.sinon = require('sinon');
