22bd9cf4bf7df7bd5de7afc9d3e4d6dad802f1f0
Sjur Bakka <barsju@gmail.com>
2017-03-24 23:14:08 +0100

    Validate input args

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 const term = require('terminal-kit').terminal,
   FileHandle = require('./lib/file-handle'),
   main = require('./lib/main');
-
 const args = require('minimist')(process.argv, {
   boolean: ['s', 'c'],
   alias: {
@@ -15,21 +14,26 @@ const args = require('minimist')(process.argv, {
   }
 });
 
+if (args._.length < 3) {
+  console.error('No input file specified.');
+  process.exit(1);
+}
+
+const file = args._[args._.length - 1];
+
 let marker = args.marker;
 if (!marker && process.platform === 'win32') {
   // Windows CMD and PowerShell dosn't support ANSI Inverse.
   marker = '^Y';
 }
-
 const progArgs = {
   status: args.status,
   keys: args.keys,
   colors: args.colors,
   selectMarker: marker || '^!',
-  file: new FileHandle(args._[args._.length - 1]),
+  file: new FileHandle(file),
   term: term
 };
-
 main(progArgs, (err) => {
   let status = 0;
   if (err) {
